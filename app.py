09a2a05ddc323dd13bc9c716d45770b5707a98f7
mtomi <mtomi@student.tgm.ac.at>
2023-05-08 17:31:09 +0200

    File runterladen möglich

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-
+from flask import Flask, send_file
+from flask import render_template
+from flask import request
+from markupsafe import escape
 app = Flask(__name__)
-cors = CORS(app)
-
-@app.route('/data', methods=['GET'])
-def get_data():
-    data = {
-        'message': 'Hallo von Flask!'
-    }
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        return f'Hello {escape (username)}!'
+    else:
+        return render_template('login.html')
+    
+@app.route("/Trainingsplan", methods=["GET", "POST"])
+def Trainingsplan():
+     filepath = '/Users/johnyhamsik12/Desktop/Entschuldigungen Tomi_freitag.pdf'
+     return send_file(filepath, as_attachment=True)
