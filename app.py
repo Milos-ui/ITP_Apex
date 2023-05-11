906530d8831f3f6dc1721386fd2d0c3d34578f67
mmatic64 <mmatic@student.tgm.ac.at>
2023-05-11 13:46:19 +0200

    Flask geändert

--- app.py
+++ app.py
@@ -3,17 +3,25 @@ from flask import render_template
 from flask import request
 from markupsafe import escape
 app = Flask(__name__)
+daten = {}
 
 @app.post('/trainingsplan')
 def trainingsplan_post():
-    sportDrop = request.form['sportDrop']
-    timeInvest = request.form['timeInvest']
-
+    daten = {
+        'sportDrop': request.form['sportDrop'],
+        'timeInvest': request.form['timeInvest'],
+        'goal': request.form['goal'],
+        'weight': request.form['weight'],
+        'height': request.form['height'],
+        'age': request.form['age'],
+        'gender': request.form['gender'],
+        'fitLevel': request.form['fitLevel']
+    }
+    
     filepath = 'daten/Trainingsplan.pdf'
     return send_file(filepath, as_attachment=True)
 
 
 @app.get('/trainingsplan')
 def trainingsplan_get():
-    filepath = 'daten/Trainingsplan.pdf'
-    return send_file(filepath, as_attachment=True)
+    return json.dump(daten)
