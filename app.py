d1f555c3077cc8c611a7b8b03d7798ccf53d4b00
mmatic64 <mmatic@student.tgm.ac.at>
2023-05-11 13:31:21 +0200

    index in indexTest kopiert und für backend angepasst

--- app.py
+++ app.py
@@ -3,20 +3,17 @@ from flask import render_template
 from flask import request
 from markupsafe import escape
 app = Flask(__name__)
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form['username']
-        benutzer = {
-            'Alter': request.form['alter'],
-            'Geschlecht': request.form['geschlecht']
-        }
-        return f'Hello {escape (username)}!'
-    else:
-        return render_template('login.html')
-    
-@app.route("/Trainingsplan", methods=["GET", "POST", "DOWNLOAD"])
-def Trainingsplan():
-     if request.method == "DOWNLOAD":
-        filepath = 'daten/Trainingsplan.pdf'
-        return send_file(filepath, as_attachment=True)
+
+@app.post('/trainingsplan')
+def trainingsplan_post():
+    sportDrop = request.form['sportDrop']
+    timeInvest = request.form['timeInvest']
+
+    filepath = 'daten/Trainingsplan.pdf'
+    return send_file(filepath, as_attachment=True)
+
+
+@app.get('/trainingsplan')
+def trainingsplan_get():
+    filepath = 'daten/Trainingsplan.pdf'
+    return send_file(filepath, as_attachment=True)
