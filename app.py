6973d924677ee2b32a63a94b7c88a848510f5896
mtomi <mtomi@student.tgm.ac.at>
2023-05-15 17:37:30 +0200

    Datenbakn und Login Anfang

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import render_template
 from flask import request
 from markupsafe import escape
 import json
+
+import mysqlx
 app = Flask(__name__)
 daten = {}
 
 @app.post('/trainingsplan')
 def trainingsplan_post():
+    # Überprüfen, ob alle Formulardaten vorhanden sind
     if(request.form.get('sportDrop') == None or request.form.get('timeInvest') == None or request.form.get('goal') == None or request.form.get('weight') == None
     or request.form.get('height') == None or request.form.get('age') == None or request.form.get('gender') == None or request.form.get('fitLevel') == None):
         print('Error: Nicht alle Felder ausgefüllt')
         return 'Error: Nicht alle Felder ausgefüllt'
     
+    # Speichern der Formulardaten im "daten" Dictionary
     daten = {
         'sportDrop': request.form['sportDrop'],
         'timeInvest': request.form['timeInvest'],
@@ -24,6 +28,7 @@ def trainingsplan_post():
         'fitLevel': request.form['fitLevel']
     }
 
+    # Festlegen des Dateipfads basierend auf der ausgewählten Sportart
     if(daten['sportDrop'] == 'Laufen'):
         filepath = 'daten/Trainingsplan1.pdf'
     elif(daten['sportDrop'] == 'Radfahren'):
@@ -36,9 +41,35 @@ def trainingsplan_post():
         print('Error: Sportart nicht angegeben')
         return 'Error: Sportart nicht angegeben'
 
+    # Datei als Anhang zurückgeben
     return send_file(filepath, as_attachment=True)
 
 
 @app.get('/trainingsplan')
 def trainingsplan_get():
     return json.dump(daten)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'  # Benutzername der Datenbank
+app.config['MYSQL_PASSWORD'] = 'admin'  # Passwort der Datenbank
+app.config['MYSQL_DB'] = 'Login'  # Name der Datenbank
+mysql = mysqlx(app)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Daten aus dem Formular abrufen
+        username = request.form['username']
+        password = request.form['password']
+
+        # Daten in der Datenbank speichern
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        mysql.connection.commit()
+        cursor.close()
+
+        # Erfolgsmeldung anzeigen
+        return 'Registrierung erfolgreich'
+
+    # HTML-Formular anzeigen
+    return render_template('login.html')
